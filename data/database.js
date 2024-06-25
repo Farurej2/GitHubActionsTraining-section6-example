d2a84a697c094ace8ef25482c32fcc3f989dce0c
pgryff <gryffpawel@gmail.com>
2024-06-25 14:56:19 +0200

    reverted to the original database.js content

--- data/database.js
+++ data/database.js
@@ -1,20 +1,12 @@
-import { MongoClient, ServerApiVersion  } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
-// const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`;
-const uri = `mongodb+srv://farurej2:${dbPassword}@cluster0.0siwlpr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
 
